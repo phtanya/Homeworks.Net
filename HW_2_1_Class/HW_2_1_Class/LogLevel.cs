e432ad83fda74f5bb707e1affe5e219c378c4ad5
Tanya Phan <phan.tttanya@gmail.com>
2022-12-12 17:19:35 +0100

    Added Homework 1 Module 2

---
+++ HW_2_1_Class/HW_2_1_Class/LogLevel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_1_Class
+{
+    public class LogLevel
+    {
+        private string? _message;
+        private LogType _type;
+        private DateTime _time;
+
+        public LogLevel(DateTime time, string? message, LogType type)
+        {
+            _time = time;
+            _message = message;
+            _type = type;
+        }
+
+        public DateTime Time
+        {
+            get { return _time; }
+        }
+
+        public string? Message
+        {
+            get { return _message; }
+        }
+
+        public LogType Type
+        {
+            get { return _type; }
+        }
+    }
+}
---
+++ HW_2_1_Class/HW_2_1_Class/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_1_Class
+{
+    internal class Logger
+    {
+        private LogLevel[] _storage = new LogLevel[20];
+        private int _index = 0;
+        private static Logger? _instance;
+
+        private Logger()
+        {
+        }
+
+        public LogLevel[] Storage
+        {
+            get { return _storage; }
+        }
+
+        public static Logger GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Logger();
+            }
+
+            return _instance;
+        }
+
+        public void Log(LogLevel item)
+        {
+            _storage[_index] = item;
+            _index++;
+        }
+    }
+}
