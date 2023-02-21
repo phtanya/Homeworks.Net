1fb476baab8bbcc67ce1b32de4da2520ae4b1037
Tanya Phan <phan.tttanya@gmail.com>
2023-02-21 13:25:33 +0100

    HomeWork 5 Module 4 - Lazy Loading LINQ

---
+++ HW_4_3_CodeFirst/HW_4_3_CodeFirst/Models/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4_3_CodeFirst.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string CompanyName { get; set; }
+        public string Location { get; set; }
+        public DateTime FoundedDate { get; set; }
+        public string Email { get; set; }
+        public virtual List<Project> Project { get; set; } = new List<Project>();
+    }
+}
---
+++ HW_4_3_CodeFirst/HW_4_3_CodeFirst/Models/Title.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4_3_CodeFirst.Models
+{
+    public class Title
+    {
+        public int TitleId { get; set; }
+        public string Name { get; set; }
+        public virtual List<Employee> Employee { get; set; } = new List<Employee>();
+    }
+}
